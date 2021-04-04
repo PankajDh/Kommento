742898e795a0f095a4c442aa3feb43acb282121d
Pankaj Dhariwal <pankajdhariwal@Pankajs-MacBook-Air.local>
2021-04-04 12:15:48 +0530

    add code push and app center

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import 'react-native-gesture-handler';
 import StackNavigatorLogin from './navigation/StackNavigatorLogin';
+import codePush from 'react-native-code-push';
 
-const App = () => {
+let App = () => {
   return (
     <NavigationContainer>
       <StackNavigatorLogin />
     </NavigationContainer>
   );
 };
-
+App = codePush(App);
 export default App;
